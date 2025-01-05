769ec9bcc6f4b21b6200850477d10687f36a7909
Valeriia Pylypenko <pylypenko.valeriia1@gmail.com>
2025-01-05 16:40:05 +0000

    Build project

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig(({ command }) => {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
+    base: '/goit-js-hw-09/',
     root: 'src',
     build: {
       sourcemap: true,
