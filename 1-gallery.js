5de7ef97d6238f428f9b5e3c6a9818ba2d984a60
valeriia123 <pylypenko.valeriia1@gmail.com>
2025-01-05 18:48:55 +0000

    Deploying to gh-pages from @ valeriia123/goit-js-hw-09@df3f2676be25436ee4b05f7c53e896ec51bd9ec5 🚀

---
+++ 1-gallery.js
@@ -0,0 +1,2 @@
+import"./assets/style-B17TD1K1.js";import{S as a}from"./assets/vendor-B-WyX8ve.js";new a(".js-gallery a",{captionsData:"alt",captionDelay:250});
+//# sourceMappingURL=1-gallery.js.map
--- assets/1-gallery-DCGldUfN.js
+++
@@ -1,2 +0,0 @@
-import{S as a}from"./vendor-B-WyX8ve.js";new a(".js-gallery a",{captionsData:"alt",captionDelay:250});
-//# sourceMappingURL=1-gallery-DCGldUfN.js.map
